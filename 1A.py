1d8ac32488ac32f52ed66493d177ca59b3dd46d6
atacoban <acobanx@gmail.com>
2024-02-24 11:32:26 +0300

    Code formatting

--- 1A.py
+++ 1A.py
@@ -1,11 +1,11 @@
-a,b,c=map(int,input().split())
-if a%c==0:
-    d=int(a/c)
+a, b, c = map(int, input().split())
+if a % c == 0:
+    d = int(a/c)
 else:
-    d=int(a/c)+1
+    d = int(a/c)+1
 
-if b%c==0:
-    f=int(b/c)
+if b % c == 0:
+    f = int(b/c)
 else:
-    f=int(b/c)+1
+    f = int(b/c)+1
 print(f*d)
--- 9A.py
+++ 9A.py
@@ -1,25 +1,17 @@
-a=list(map(int,input().split()))   
+a = list(map(int, input().split()))
 a.sort()
-b=((6-a[1])+1)
-if b==0:
+b = ((6-a[1])+1)
+if b == 0:
     print("0/1")
-if b==1:
+if b == 1:
     print("1/6")
-if b==2:
+if b == 2:
     print("1/3")
-if b==3:
+if b == 3:
     print("1/2")
-if b==4:
+if b == 4:
     print("2/3")
-if b==5:
+if b == 5:
     print("5/6")
-if b==6:
+if b == 6:
     print("1/1")
-
-
-
-
-
-
-
-
