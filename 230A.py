c47f869d87e9be8bcdcdc760033853139c6edbdf
atacoban <acobanx@gmail.com>
2024-02-27 15:46:13 +0300

    230A successful

--- 230A.py
+++ 230A.py
@@ -1,24 +1,16 @@
-ata,n=map(int,input().split())
-a=[]
-b=0
-for i in range (int(n)):
-    a.append(list(map(int,input().split())))
+ata, n = map(int, input().split())
+a = []
+b = 0
+for i in range(int(n)):
+    a.append(list(map(int, input().split())))
 a.sort()
 
 for i in a:
-    if ata>i[0]:
-        ata+=i[1]
+    if ata > i[0]:
+        ata += i[1]
     else:
-        b+=1
+        b += 1
         print("NO")
         break
-if b==0:
+if b == 0:
     print("YES")
-
-
-
-
-
-
-
-
