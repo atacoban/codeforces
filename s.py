c65a7488c7523a38cae235821aee1a00f5530aa7
atacoban <acobanx@gmail.com>
2024-03-07 21:07:45 +0300

    BİNARY SEARCH

---
+++ s.py
@@ -0,0 +1,35 @@
+# It returns location of x in given array arr
+def binarySearch(arr, l, r, x):
+ 
+    while l <= r:
+ 
+        mid = l + (r - l) // 2
+        print(mid)
+        # Check if x is present at mid
+        if arr[mid] == x:
+            return mid
+ 
+        # If x is greater, ignore left half
+        elif arr[mid] < x:
+            l = mid + 1
+ 
+        # If x is smaller, ignore right half
+        else:
+            r = mid - 1
+ 
+    # If we reach here, then the element
+    # was not present
+    return -1
+ 
+ 
+# Driver Code
+if __name__ == '__main__':
+    arr = [x for x in range(101)]
+    x = 68
+ 
+    # Function call
+    result = binarySearch(arr, 0, len(arr)-1, x)
+    if result != -1:
+        print("Element is present at index", result)
+    else:
+        print("Element is not present in array")
